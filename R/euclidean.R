be2ac5562e4c9bca36fde494d6f09883ba9b3dba
Malma831 <89968327+Malma831@users.noreply.github.com>
2021-09-03 10:18:37 +0200

    uppdate

---
+++ R/euclidean.R
@@ -0,0 +1,34 @@
+#' Title
+#'
+#' @param x1
+#' @param x2
+#'
+#' @return
+#' @export
+#'
+#' @examples
+euclidean <- function(x1, x2){
+
+  if(!is.numeric(x1)){
+    stop("x1 is not numeric")
+  } else if(x1 %% 1 != 0) {
+    stop("x1 is not an integer")
+  }
+
+  if(!is.numeric(x2)){
+    stop("x2 is not numeric")
+  } else if(x2 %% 1 != 0){
+    stop("x2 is not an integer")
+  }
+
+
+  while(x2 != 0){ #x2 är b och x1 är a från pseudokod.
+    t <- x2
+    x2 <-  x1 %% x2
+    x1 <- t
+
+
+  }
+
+  return(x1)
+}
