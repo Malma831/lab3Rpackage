125969e63d9fad15ee4b007d16c8e6749913699d
Malma831 <89968327+Malma831@users.noreply.github.com>
2021-09-07 14:06:18 +0200

    Update euclidean.R

--- R/euclidean.R
+++ R/euclidean.R
@@ -9,19 +9,7 @@
 #' @examples euclidean(100, 1000)
 
 euclidean <- function(x1, x2){
-
-  if(!is.numeric(x1)){
-    stop("x1 is not numeric")
-  } else if(x1 %% 1 != 0) {
-    stop("x1 is not an integer")
-  }
-
-  if(!is.numeric(x2)){
-    stop("x2 is not numeric")
-  } else if(x2 %% 1 != 0){
-    stop("x2 is not an integer")
-  }
-
+  stopifnot(!is.numeric(x) | !is.numeric(y) | length(x)!=1 | length(y)!=1)
 
   while(x2 != 0){
     t <- x2
